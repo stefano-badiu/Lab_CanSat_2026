1fa04f11a9cb99b406a75025a440b036579ed4ef
Valerio0409 <favaloma0104@gmail.com>
2026-04-17 16:21:56 +0200

    aggiunta calibrazione

--- include/MPU6050.hpp
+++ include/MPU6050.hpp
@@ -1,7 +1,6 @@
 #pragma once
-
 #include "Sputnik_Identity.hpp"
-
+#include <stdint.h>
 extern Telemetry current_data;
 
 bool init_MPU6050();
@@ -23,4 +22,3 @@ bool is_MPU6050_calibrated();
 void reset_MPU6050_calibration();
 MPU6050CalibrationData get_MPU6050_calibration_data(); // Restituisce i dati di calibrazione attuali, se validi. Se non calibrato, restituisce valori di default con valid=false.
 void set_MPU6050_calibration_data(const MPU6050CalibrationData& calibData); // Permette di impostare manualmente i dati di calibrazione, ad esempio da valori salvati in precedenza. Se calibData.valid è false, ignora i dati e non applica alcuna calibrazione.
-
